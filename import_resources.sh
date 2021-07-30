03c7a36f6ac4d5933edf005d1b7e8def3fe85100
hs-llu <lucy.lu@homestars.com>
2021-07-30 11:37:28 -0400

    check state of resources before importing

--- import_resources.sh
+++ import_resources.sh
@@ -6,7 +6,9 @@ then
   do
     resource_name="$(echo $line | cut -d':' -f 1)"
     resource_id="$(echo $line | cut -d':' -f 2)"
-    terraform import $resource_name $resource_id
+    if [[ -z $(terragrunt state list $resource_name) ]];  then
+      terragrunt import $resource_name $resource_id
+    fi
   done < resources.yaml
 else
   echo "No resources to import"
