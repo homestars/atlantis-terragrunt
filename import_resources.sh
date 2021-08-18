c832fbdf1275396017f4f7a96c6ba521169b2fcc
hs-llu <lucy.lu@homestars.com>
2021-08-18 16:33:56 -0400

    use regex to match resource name

--- import_resources.sh
+++ import_resources.sh
@@ -2,16 +2,17 @@
 resources_file=resources.yaml
 if [ -f "$resources_file" ];
 then
-  state_list=$(terragrunt state list)
+  state_list="$(terragrunt state list)"
   if [ $? -eq 1 ]; then state_list=''; fi
   while IFS= read -r line
   do
     resource_name="$(echo $line | cut -d':' -f 1)"
-    resource_id="$(echo $line | cut -d':' -f 2)"
-    if echo $state_list | grep -q $resource_name;  then
-      echo "Skip importing $resource_name as the state already exists"
+    resource_id="$(echo $line | cut -d':' -f 2-)"
+    if [[ "$state_list" =~ .*"$resource_name".* ]];  then
+      echo "Skip importing as state of $resource_name already exists"
     else
       terragrunt import $resource_name $resource_id
+      if [ $? -eq 0 ]; then echo "$resource_name is imported"; else echo "$resource_name is not imported" ; fi
     fi
   done < resources.yaml
 else
